151e1bda383c0cf29c28258c8d67cc29bfdca5d6
athirasarman <kaithakkottuathira@gmail.com>
2021-06-23 15:56:54 +0530

    module structure

--- Solution/src/app/app-routing.module.ts
+++ Solution/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
 
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }];
+const routes: Routes = [
+{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+{ path: '', redirectTo:'layout', pathMatch:'full'},];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+   imports: [RouterModule.forRoot(routes, { enableTracing: false, // <-- debugging purposes only
+                                           preloadingStrategy: SelectivePreloadingStrategyService })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Solution/src/app/app.module.ts
+++ Solution/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    DashboardModule,
     AppRoutingModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
